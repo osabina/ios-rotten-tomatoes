45e8aa6365c07c149badb2897e8f7ebdbffa2fcf
Ozzie Sabina <oz@sabina.org>
2014-06-16 18:41:45 -0400

    ScrollView now scrolls!

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Rotten Tomatoes app for iOS class
 - User can view a list of movies from Rotten Tomatoes.  Poster images must be loading asynchronously.
 - User can pull to refresh the movie list.
 - User sees loading state while waiting for movies API.
-- User sees error message when there's a networking error.  You may not use UIAlertView to display the error.  (Not done exactly as spec'ed)
+- User sees error message when there's a networking error.  You may not use UIAlertView to display the error. 
 - User can view movie details by tapping on a cell
 - Add a tab bar for Box Office and DVD. (optional)
 
@@ -27,23 +27,11 @@ Rotten Tomatoes app for iOS class
 
 **Notes:**
 
-This took me about 12 hours +/- a couple of hours.  At least a third of that time was trying to get
-CocoaPods to work due to linking errors.  I finally realized it was building for 'Debug' but trying to
-link for 'Release'.  I literally was banging my head against the desk with this.  
+This took me about 15 hours +/- a couple of hours.
 
-Since so little was covered in class, most of the remaining time was spent tracking down
-how to do many of the things that we needed to do that were not even touched upon, rather than writing
-and debugging.  I noted that the SF class has a lab period as well as lecture - is this class going to
-just be too accelerated for folks like me who have no experience with mobile development beforehand?
+Things that drove me the most nuts (spent hours figuring out, 1 minute to fix once I did):
+  - Getting CocoaPods to link with the proper build (building Debug, trying to link Release)
+  - Figuring out why UIScrollView wouldn't scroll (xib auto layout checkbox was checked)
 
-One UI nit I noticed and didn't manage to fix is that the list view needs to be pulled "hard"
-to see the movies at the front and end.  This is particularly a problem at the front where it's
-easy to pull too hard and cause a refresh (and a UI jump back down).  Would love to hear how to fix
-this.
-
-I also couldn't figure out how to get the scrollable detail view as in the prototype demo after going
-too far down that rathole, so I instead implemented a more useful page of detailed info that was static.
-Ditto above message about how to do this.
-
-Also couldn't figure out the intent for the network error message, so I used a control instead.  And
-again, ditto would love clarification.
+I used a CocoaPod control for the network error message, wasn't clear on the intent of the
+requirement to not use UIAlertView.
--- RottenTomatoes/RottenTomatoes/DetailsViewController.m
+++ RottenTomatoes/RottenTomatoes/DetailsViewController.m
@@ -14,9 +14,11 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *criticScoreLabel;
 @property (weak, nonatomic) IBOutlet UILabel *audienceScoreLabel;
-
+@property (weak, nonatomic) IBOutlet UIScrollView *synopsisScrollView;
+@property (weak, nonatomic) IBOutlet UIView *contentView;
 @property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *posterImage;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 
 @end
 
@@ -34,27 +36,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    NSDictionary *ratings = self.movie[@"ratings"];
 
     self.title= self.movie[@"title"];
-    self.synopsisLabel.text = self.movie[@"synopsis"];
-    [self.synopsisLabel setNumberOfLines:0];
-    [self.synopsisLabel sizeToFit];
-    NSString *criticRating = [NSString stringWithFormat:@"%@ (%@%%)",
-                ratings[@"critics_rating"], ratings[@"critics_score"]];
-    NSString *audienceRating = [NSString stringWithFormat:@"%@ (%@%%)",
-                              ratings[@"audience_rating"],
-                              ratings[@"audience_score"]];
-    self.criticScoreLabel.text = criticRating;
-    self.audienceScoreLabel.text = audienceRating;
-    
-    //   [self.overlayView addSubview:self.synopsisLabel];
 
     NSURL *posterUrl = [NSURL URLWithString:[self.movie valueForKeyPath:@"posters.detailed"]];
     [self.posterImage setImageWithURL:posterUrl placeholderImage:nil];
+
     
+    self.titleLabel.text = self.movie[@"title"];
+    self.synopsisLabel.text = self.movie[@"synopsis"];
+    [self.synopsisLabel sizeToFit];
+    
+    NSDictionary *ratings = self.movie[@"ratings"];
+    NSString *criticRating = [NSString stringWithFormat:@"%@ (%@%%)",
+                              ratings[@"critics_rating"], ratings[@"critics_score"]];
+    NSString *audienceRating = [NSString stringWithFormat:@"%@ (%@%%)",
+                                ratings[@"audience_rating"],
+                                ratings[@"audience_score"]];
+    self.criticScoreLabel.text = criticRating;
+    self.audienceScoreLabel.text = audienceRating;
+    
+    [self.contentView sizeToFit];
+    // Found that making the width smaller stops horizontal scrolling
+    [self.synopsisScrollView setContentSize:CGSizeMake(self.contentView.frame.size.width -10 ,
+                                                       self.contentView.frame.size.height + 200)];
+
+    // add container view to scroll view
+    self.synopsisScrollView.frame = self.view.frame;
+
 }
+
 
 - (void)didReceiveMemoryWarning
 {
